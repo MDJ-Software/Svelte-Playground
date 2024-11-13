75b960349659c66e3f218ab92665982a3e5848b4
Matthew Jensen <mattydjensen@gmail.com>
2024-11-13 12:54:19 -0500

    Create ClickableTile.svelte

---
+++ ClickableTile.svelte
@@ -0,0 +1,15 @@
+<script>
+	let props = $props();
+	function onmouseover() {
+		return 0;
+	}
+</script>
+
+<button {...props}></button>
+
+<style>
+	button {
+		height: 20%;
+		border-style: none
+	}
+</style>
